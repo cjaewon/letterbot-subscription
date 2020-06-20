7f7195c6c8d595cb8553c73c7176d80ffb2bf6b9
cjaewon <jwn4492@outlook.com>
2020-06-20 13:54:18 +0900

    Create bio

--- client/scripts/index.ts
+++ client/scripts/index.ts
@@ -1,2 +1,14 @@
 import '../styles/global.css';
+import twemoji from 'twemoji';
 
+twemoji.parse(document.body, {
+  callback: function(icon, options, variant) {
+      switch ( icon ) {
+          case 'a9':      // © copyright
+          case 'ae':      // ® registered trademark
+          case '2122':    // ™ trademark
+              return false;
+      }
+      return ''.concat(options.base, options.size, '/', icon, options.ext);
+  }
+});
